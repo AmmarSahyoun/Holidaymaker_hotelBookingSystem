5b1a1fc138e6e7ed079733eacf17770edcc70cd6
Ammar Sahyoun <ammarzgr@hotmail.com>
2020-02-23 23:41:21 +0100

    change or cancel a booking

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -18,16 +18,14 @@ public class Program {
     public Program() {
         boolean startUserMenu = true;
 
-
         while (startUserMenu) {
 
             System.out.println();
             System.out.println("    .:Welcome Admin to the Holidaymaker:.");
-            System.out.println("[1] register a new customer ");
-            System.out.println("[2] search for available room and Book");
-            System.out.println("[3] change a room booking");
-            System.out.println("[4] cancel a room booking");
-            System.out.println("[5] search for customer");
+            System.out.println("[1] Register a new customer ");
+            System.out.println("[2] Search for available room and Book");
+            System.out.println("[3] Change a booking");
+            System.out.println("[4] Search for a customer info");
             System.out.println("[0] Exit");
 
             try {
@@ -61,23 +59,23 @@ public class Program {
                 checkIn = Date.valueOf(scn.nextLine());
                 System.out.println("Enter check out date(yyyy-mm-dd):");
                 checkOut = Date.valueOf(scn.nextLine());
-                System.out.println("choose your hotel facilities:");
-                System.out.println("[1] with pool  [0] without pool");
+                System.out.println("Choose your hotel facilities:");
+                System.out.println("[1] With pool  [0] Without pool");
                 pool = Boolean.parseBoolean(scn.nextLine());
-                System.out.println("[1] with restaurant  [0] without restaurant");
+                System.out.println("[1] With restaurant  [0] Without restaurant");
                 restaurant = Boolean.parseBoolean(scn.nextLine());
-                System.out.println("[1] with children activities  [0] without children activities");
+                System.out.println("[1] With children activities  [0] Without children activities");
                 childrenActivities = Boolean.parseBoolean(scn.nextLine());
-                System.out.println("[1] with entertainment  [0] without entertainment");
+                System.out.println("[1] With entertainment  [0] Without entertainment");
                 entertainment = Boolean.parseBoolean(scn.nextLine());
 
                 searchForRoom(checkIn, checkOut, pool, restaurant, childrenActivities, entertainment);
 
                 int guestID = 0;
-                System.out.println("choose a room ID to book:");
+                System.out.println("Choose a room ID to book:");
                 int roomID = scn.nextInt();
-                System.out.println("are you registered as customer?");
-                System.out.println("[1] NO, i want to register [2] enter your customer id:");
+                System.out.println("Are you registered as a hotel guest?");
+                System.out.println("[1] NO, i want to register    [2] Enter your customer id:");
                 try {
                     choice = scn.nextInt();
                 } catch (NumberFormatException ex) {
@@ -89,42 +87,39 @@ public class Program {
                 }
                 if (choice == 1) {
                     guestID = registerNewCustomer();
-                }
-                else if (choice == 2) {
+                } else if (choice == 2) {
                     guestID = scn.nextInt();
                 }
 
                 System.out.println("Do you need extra bed (Y/n)?");
 
-                scn = new Scanner (System.in);
+                scn = new Scanner(System.in);
                 String result = scn.nextLine();
                 boolean extraBed = (result.equalsIgnoreCase("y") ? true : false);
 
-                System.out.println("How many traveler?");
+                System.out.println("How many guests companion you?");
                 int companion = scn.nextInt();
 
-                scn = new Scanner (System.in);
+                scn = new Scanner(System.in);
                 System.out.println("Full board or Half board meal (full/half)?");
                 String mealBoard = scn.nextLine();
 
-                if (mealBoard.equalsIgnoreCase("full")){
+                if (mealBoard.equalsIgnoreCase("full")) {
                     mealBoard = "fullBoard";
                 } else {
                     mealBoard = "halfBoard";
                 }
 
-                if (guestID != 0){
+                if (guestID != 0) {
                     bookRoom(checkIn, checkOut, guestID, roomID, extraBed, companion, mealBoard);
                 }
 
             }
             if (choice == 3) {
-
+                changeBooking();
             }
+
             if (choice == 4) {
-
-            }
-            if (choice == 5) {
                 searchForCustomer();
             }
 
@@ -139,7 +134,7 @@ public class Program {
         int bookingID = 0;
         try {
 
-            statement = conn.prepareStatement("Insert Into Booking (checkIn, checkOut, companion, guest_id)" + " VALUES(?, ?, ?, ?)",Statement.RETURN_GENERATED_KEYS);
+            statement = conn.prepareStatement("Insert Into Booking (checkIn, checkOut, companion, guest_id)" + " VALUES(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
             statement.setDate(1, checkIn);
             statement.setDate(2, checkOut);
             statement.setInt(3, companion);
@@ -153,25 +148,23 @@ public class Program {
                 try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
                     if (generatedKeys.next()) {
                         bookingID = generatedKeys.getInt(1);
-                    }
-                    else {
-                        throw new SQLException("Creating user failed, no ID obtained.");
+                    } else {
+                        throw new SQLException("Operation failed!");
                     }
                 }
 
-                if (bookingID != 0)
-                {
+                if (bookingID != 0) {
                     System.out.println(bookingID);
                     PreparedStatement statementRooms = conn.prepareStatement("Insert Into booking_x_rooms (booking_id, rooms_id, extraBed, meals)" + " VALUES(?, ?, ?, ?)");
                     statementRooms.setInt(1, bookingID);
                     statementRooms.setInt(2, roomID);
                     statementRooms.setBoolean(3, extraBed);
-                    statementRooms.setInt(4, (mealBoard.equalsIgnoreCase("fullboard")? 1 : 0));
+                    statementRooms.setInt(4, (mealBoard.equalsIgnoreCase("fullboard") ? 1 : 0));
 
                     int roomInsert = statementRooms.executeUpdate();
 
                     if (roomInsert != 0) {
-                        System.out.println("Room assigned SUCCESS");
+                        System.out.println("The room has booked successfully");
                     }
                 }
             } else System.out.println("Something were wrong!");
@@ -196,7 +189,7 @@ public class Program {
         String cusMobile = scn.nextLine();
         try {
 
-            statement = conn.prepareStatement("INSERT INTO guest (guestName, guestAddress, guestEmail, guestMobile)" + " VALUES(?, ?, ?, ?)",Statement.RETURN_GENERATED_KEYS);
+            statement = conn.prepareStatement("INSERT INTO guest (guestName, guestAddress, guestEmail, guestMobile)" + " VALUES(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, cusName);
             statement.setString(2, cusAdd);
             statement.setString(3, cusEmail);
@@ -210,8 +203,7 @@ public class Program {
                 try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
                     if (generatedKeys.next()) {
                         guestID = generatedKeys.getInt(1);
-                    }
-                    else {
+                    } else {
                         throw new SQLException("Creating user failed, no ID obtained.");
                     }
                 }
@@ -244,7 +236,7 @@ public class Program {
             sqlStat = sqlStat + " and entertainment = 1";
         }
 
-        sqlStat = sqlStat +" Order By HotelName, RoomNr";
+        sqlStat = sqlStat + " Order By HotelName, RoomNr";
 
         try {
             statement = conn.prepareStatement(sqlStat);
@@ -278,6 +270,64 @@ public class Program {
         }
     }
 
+    public void changeBooking() {
+        int guestId = 0;
+        System.out.println("enter a guest id:");
+        guestId = scn.nextInt();
+        try {
+            statement = conn.prepareStatement("select * from allbooking_view \n" +
+                    "\tInner Join Guest on (Guest.id = allbooking_view.guestID)\n" +
+                    "    Inner Join AllRooms_View on (AllRooms_View.roomid = allbooking_view.roomid) where guestID like ?");
+            statement.setString(1, String.valueOf(guestId));
+
+            resultSet = statement.executeQuery();
+        } catch (
+                Exception e) {
+
+            e.printStackTrace();
+        }
+        try {
+            while (resultSet.next()) {
+                String row = " Id: " + resultSet.getString("id")
+                        + ", Name: " + resultSet.getString("guestName")
+                        + ", booking id: " + resultSet.getString("bookingId")
+                        + ", check-in: " + resultSet.getString("checkIn")
+                        + ", check-out: " + resultSet.getString("checkOut")
+                        + ", companion: " + resultSet.getString("companion")
+                        + ", extra Bed: " + resultSet.getString("extraBed")
+                        + ", meals: " + resultSet.getString("meals")
+                        + ", room Id: " + resultSet.getString("roomId")
+                        + ", room type: " + resultSet.getString("roomType")
+                        + ", room price: " + resultSet.getString("roomPrice");
+
+                System.out.println(row);
+                System.out.println();
+            }
+        } catch (
+                Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("[1]Change booking's meals   [2]Add/remove extra bed    [3]Cancel the booking   [4]Back to main menu");
+        try {
+            choice = scn.nextInt();
+        } catch (NumberFormatException ex) {
+            System.err.println("Choose a valid number");
+            return;
+        } catch (Exception e) {
+            System.out.println("Not a valid number!!");
+            return;
+        }
+        if (choice == 1) {
+            // changeMeals();
+        }
+        if (choice == 2) {
+            //  changeExtraBed();
+        }
+        if (choice == 3) {
+
+        }
+
+    }
 
     public void searchForCustomer() {
 
